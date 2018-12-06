732f4d5a64ca59b11ec13722ea5e6ea7d635fa68
Dogchamp <chrischrisquion@gmail.com>
2018-12-06 11:51:46 -0800

    Small changes

--- src/discriminator_test.py
+++ src/discriminator_test.py
@@ -1,9 +1,25 @@
 import discriminator as dsc
 import tensorflow as tf
 
-dsc.get_dataset("../data/faces/lfw", 100)
-tensor = dataset.make_one_shot_iterator().get_next()
+train_ds, test_ds = dsc.get_train_test_dataset("../data/lfw-deepfunneled", 100, 5, .7)
+# train_ds_iter = train_ds.make_one_shot_iterator()
+# next_el = train_ds_iter.get_next()
 
 with tf.Session() as sesh:
-    print(sesh.run(tensor))
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    # sess.run(train_ds_iter.initializer)
+    sess.run(init_op)
+
+    image_input_layer = tf.get_default_graph().get_tensor_by_name("input")
+    embeddings_layer = tf.get_default_graph().get_tensor_by_name("embeddings")
+    phase_train = tf.get_default_graph().get_tensor_by_name("phase_train")
+    global_tep = tf.get_or_create_global_step()
+    embeddings, labels = dsc._get_embeddings(embeddings_layer, images, labels, \
+                                                image_input_layer, \
+                                                phase_train, \
+                                                sess)
+
+print(train_ds.output_classes)
+print(train_ds.output_shapes)
+print(train_ds.output_types)
 
